0c51df91139b366ae26235c7b00a5940701b75fa
Juliano Siloto Assine <julianosiloto@gmail.com>
2021-08-18 19:56:41 -0300

    working tensor communication

--- device/main.py
+++ device/main.py
@@ -1,7 +1,46 @@
 import requests
+
+import numpy as np
+from PIL import Image
+import torchvision.transforms.functional as TF
+import bitstream
+from bitstream import BitStream
+
+from functools import reduce  # Required in Python 3
+import operator
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+
+image_path = './resource/000000000161.jpg'
+
+image = Image.open(image_path)
+x = TF.to_tensor(image)
+x.unsqueeze(0)
+print(x.shape)
+
+x = x.numpy().astype(np.int8)
+print(prod(x.shape))
+print(len(x.tostring())/4)
+
+#
+#
+
+#
+res = requests.post(url='http://0.0.0.0:5000/png',
+                    data=x.tostring(),
+                    headers={'Content-Type': 'application/octet-stream'})
+
+
 with open('./resource/000000000161.jpg', 'rb') as f:
     data = f.read()
-
-res = requests.post(url='http://0.0.0.0:5000/remote',
+res = requests.post(url='http://0.0.0.0:5000/jpg',
                     data=data,
                     headers={'Content-Type': 'application/octet-stream'})
+
+# Build model
+# Load model from checkpoint
+# Load image and convert to tensor
+# Run Model (or not)
+# Serialize tensor
+# upload tensor
--- server/app.py
+++ server/app.py
@@ -1,25 +1,47 @@
 import os
 import stat
+import numpy as np
 from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.config['UPLOAD_FOLDER'] = os.path.abspath("./photo/")
-full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+jpg_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+png_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.png')
 
 @app.route('/')
 def show_index():
-    return render_template("index.html", user_image=full_filename)
+    return render_template("index.html", jpg_image=jpg_filename, png_image=png_filename)
 
-@app.route(full_filename)
-def image():
-    return send_file(full_filename, mimetype='image/jpg')
+@app.route(jpg_filename)
+def jpg_image():
+    return send_file(jpg_filename, mimetype='image/jpg')
 
-@app.route('/remote', methods=['POST'])
-def remote():
-    with open(full_filename, 'wb+') as f:
+@app.route(png_filename)
+def png_image():
+    return send_file(png_filename, mimetype='image/png')
+
+@app.route('/jpg', methods=['POST'])
+def jpg():
+    with open(jpg_filename, 'wb+') as f:
         f.write(request.data)
 
-    os.chmod(full_filename, 0o777)
+    os.chmod(jpg_filename, 0o777)
     # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
     return "Welcome to Flask!"
+
+@app.route('/png', methods=['POST'])
+def png():
+    from PIL import Image
+    data = np.fromstring(request.data, dtype=np.int8).reshape([3, 480, 640])
+    print(data.shape)
+    return "OK"
+    im = Image.fromarray()
+    im.save(jpg_filename)
+    # with open(png_filename, 'wb+') as f:
+    #     f.write(request.data)
+
+    os.chmod(jpg_filename, 0o777)
+    # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+    return "Welcome to Flask!"
+
