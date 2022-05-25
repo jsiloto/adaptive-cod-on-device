f550e88465b4fc8937c8b8889270529a3666f416
Juliano Siloto Assine <julianosiloto@gmail.com>
2022-05-25 15:08:01 -0300

    speedup

--- android-device/app/src/main/java/org/recod/acod/ExperimentRunner.java
+++ android-device/app/src/main/java/org/recod/acod/ExperimentRunner.java
@@ -1,7 +1,10 @@
 package org.recod.acod;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.PowerManager;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,25 +16,29 @@ public class ExperimentRunner {
     private PytorchModuleWrapper moduleWrapper;
     private ApiHandler apiHandler;
     private File[] imageList;
+    private PowerManager.WakeLock wakeLock;
 
     ExperimentRunner(PytorchModuleWrapper moduleWrapper, ApiHandler apiHandler, Dataset dataset){
         this.moduleWrapper = moduleWrapper;
         this.apiHandler = apiHandler;
         this.imageList = dataset.getFileList(max_images);
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::MyWakelockTag");
     }
 
     public void run() {
+        wakeLock.acquire();
         for (int i = 0; i < max_images; i++) {
-            final Runtime runtime = Runtime.getRuntime();
-            final long usedMemInKB=(runtime.totalMemory() - runtime.freeMemory()) / 1024L;
-            System.out.println(usedMemInKB);
+//            final Runtime runtime = Runtime.getRuntime();
+//            final long usedMemInKB=(runtime.totalMemory() - runtime.freeMemory()) / 1024L;
+//            System.out.println(usedMemInKB);
 
             String imageId = imageList[i].getName();
             try {
                 FileInputStream stream = new FileInputStream(imageList[i]);
                 Bitmap bitmap = BitmapFactory.decodeStream(stream);
                 QuantizedTensor qx = moduleWrapper.run(bitmap, imageId);
-                apiHandler.postSplitTensor(qx, null);
+//                apiHandler.postSplitTensor(qx, null);
                 System.out.println(String.format("ExperimentOutput: %d images", i+1));
                 stream.close();
             } catch (IOException e) {
@@ -39,5 +46,6 @@ public class ExperimentRunner {
                 e.printStackTrace();
             }
         }
+        wakeLock.release();
     }
 }
