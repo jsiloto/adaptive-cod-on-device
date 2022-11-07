8138b62334805fcfdd2513ef2b9e0b892bcbb8a9
jsilotoa <jsilotoa@uci.edu>
2022-11-07 13:15:17 -0800

    remove cuda timing

--- host/host_benchmark.py
+++ host/host_benchmark.py
@@ -35,16 +35,14 @@ def exp(model, input_shape, repetitions, device):
     dummy_input = torch.randn(input_shape, dtype=torch.float).to(device)
     timings = np.zeros((repetitions, 1))
     # MEASURE PERFORMANCE
-    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
     with torch.no_grad():
         for rep in range(repetitions):
-            starter.record()
+            start = time.time()
             _ = model(torch.unsqueeze(dummy_input[rep], dim=0))
-            ender.record()
+            end = time.time()
             # WAIT FOR GPU SYNC
-            torch.cuda.synchronize()
-            curr_time = starter.elapsed_time(ender)
-            timings[rep] = curr_time
+            curr_time = end-start
+            timings[rep] = curr_time*1000
     return timings
 
 
