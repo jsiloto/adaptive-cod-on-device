08c21f22f30ea061e0f07e341a2c79c6e8a622e3
jsilotoa <jsilotoa@uci.edu>
2022-11-16 22:02:56 -0800

    switching experiment

--- src/benchmark/switching_benchmark.py
+++ src/benchmark/switching_benchmark.py
@@ -4,12 +4,19 @@ import argparse
 import os
 import sys
 import inspect
-
-from benchmark.timing import cpu_exp
-
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
+import numpy as np
+import torch
+import pandas as pd
+from benchmark.timing import cpu_exp
+import psutil
+import time
+
+from literature_models.base.base_wrapper import BaseWrapper
+from literature_models.model_wrapper import get_all_options, eval_single_model, wrapper_dict, build_all_jit_models
+
 
 def get_argparser():
     argparser = argparse.ArgumentParser(description='On Device Experiments')
@@ -17,26 +24,6 @@ def get_argparser():
     argparser.add_argument('--name', type=str, default="Test", help='Experiment Name')
     argparser.add_argument('-switching', action='store_true', default=False, help='Experiment Name')
     return argparser
-args = get_argparser().parse_args()
-os.environ['OMP_NUM_THREADS']=str(args.cpus)
-os.environ['MKL_NUM_THREADS']=str(args.cpus)
-
-
-import numpy as np
-import torch
-import pandas as pd
-import json
-import random
-import shutil
-import time
-import traceback
-from copy import copy
-from torch import jit
-import time
-
-from literature_models.base.base_wrapper import BaseWrapper
-from literature_models.model_wrapper import get_all_options, eval_single_model, wrapper_dict, build_all_jit_models
-from torch.profiler import profile, record_function, ProfilerActivity
 
 def benchmark_model_switching(wrapperClass: BaseWrapper, device, name):
     wrapper = wrapperClass()
@@ -49,15 +36,15 @@ def benchmark_model_switching(wrapperClass: BaseWrapper, device, name):
             al = round(1000 * (time.perf_counter() - bl), 3)
             input_shape = wrapper.get_input_shape()
             timings = np.around(cpu_exp(model, input_shape, 10, device), decimals=2)
-            results[name+str(mode)] = (al, timings.tolist())
+            results[name+str(mode)] = {"disk_load": al, "timings": timings.tolist()}
     return results
 
 
 def main():
-    df = pd.DataFrame(columns=['model', 'disk_load', 'timings'])
     device = "cpu"
     device = torch.device(device)
-    torch.set_num_interop_threads(args.cpus)
+    args = get_argparser().parse_args()
+    torch.set_num_interop_threads(1)
     torch.set_num_threads(args.cpus)
 
     # Generate all model
