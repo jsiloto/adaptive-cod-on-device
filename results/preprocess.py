03a0e9eb859d9f303a2c968464282df50ddf7707
Juliano Siloto Assine <jsilotoa@uci.edu>
2022-11-25 14:18:12 -0800

    update plots

--- results/preprocess.py
+++ results/preprocess.py
@@ -1,5 +1,29 @@
 import jsonlines
 import numpy as np
+import pandas as pd
+
+
+def read_sota():
+    df = pd.read_csv("rpi4_cpus4.csv")
+
+    df['KB'] = df['KB'].div(1000).round(1)
+    df['model'] = df["model"].replace("assine2022b", "ours", regex=True)
+    df['ms'] = df['ms'].round(0).astype(int)
+    df['mAP'] = df['mAP'].round(1)
+    df = df.set_index("model")
+
+    df = df.drop(index="ours_12")
+    df = df.drop(index="ours_13")
+    df = df.drop(index="ours_21")
+    df = df.drop(index="ours_23")
+    df = df.drop(index="ours_31")
+    df = df.drop(index="ours_32")
+    df = df.drop(index="ours_41")
+    df = df.drop(index="ours_42")
+    df = df.drop(index="ours_43")
+
+    return df
+
 
 def read_jsonlines_file(filename):
     ignore_start = 5
@@ -18,8 +42,8 @@ def read_jsonlines_file(filename):
     time = get_values("time")
     results["time"] = [t - time[0] for t in time]
     results["mode"] = get_values("mode")
-    results["compute"] = [m // 10 for m in results["mode"]]
-    results["bits"] = [m % 10 for m in results["mode"]]
+    results["compute"] = [int(m // 10) for m in results["mode"]]
+    results["bits"] = [int(m % 10) for m in results["mode"]]
     # results['communication'] = get_values('rec_time')
 
     return results
